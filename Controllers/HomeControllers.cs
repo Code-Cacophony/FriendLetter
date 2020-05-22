fa6476e54a89d9d352f259a8413255e2bce6c40c
Code-Cacophony <57301331+Code-Cacophony@users.noreply.github.com>
2020-05-21 17:40:57 -0700

    add functionality

---
+++ Controllers/HomeControllers.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using FriendLetter.Models;
+
+namespace FriendLetter.Controllers
+{
+  public class HomeController : Controller
+  {
+
+    [Route("/hello")]
+    public string Hello() { return "Hello friend!"; }
+
+    [Route("/goodbye")]
+    public string Goodbye() { return "Goodbye friend."; }
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    /* [Route("/")]
+     public string Letter() { return "Our virtual postcard will go here soon!"; }*/
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
+
+    // [Route("/")]
+    [Route("/")]
+    public ActionResult Letter()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
+      return View(myLetterVariable);
+    }
+  }
+}
